2c5c6a4b81feefedf3703b97a1064ff40266c421
Daniel Hartnell <danielhartnell@gmail.com>
2016-01-15 22:32:17 -0800

    include basic functionality

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function showDiv() {
+  if (document.getElementById("hide-div") !== null) {
+    document.getElementById("hide-div").setAttribute("id", "show-div");
+  }
+  else {
+    document.getElementById("show-div").setAttribute("id", "hide-div");
+  }
+}
+
+function addEquations() {
+  // Do some stuff
+}
