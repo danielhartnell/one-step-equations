cfcb0373e1f2633b6bd2ecaf10d5f7729d4a1775
Daniel Hartnell <danielhartnell@gmail.com>
2016-01-16 11:56:40 -0800

    kinda sorta working

--- js/main.js
+++ js/main.js
@@ -1,14 +1,55 @@
 function showDiv() {
-  if (document.getElementById("hide-div") !== null) {
-    document.getElementById("hide-div").setAttribute("id", "show-div");
+  // Set variables
+  var hide = document.getElementById("hide-div");
+  var show = document.getElementById("show-div");
+
+  // Change div ID so content is no longer hidden
+  if (hide !== null) {
+    hide.setAttribute("id", "show-div");
+    showEquations()
   }
   else {
-    document.getElementById("show-div").setAttribute("id", "hide-div");
+    show.setAttribute("id", "hide-div");
   }
-  
-  for (i = 0; i < 5; i++) {
-    var rand = Math.floor(Math.random() * (100 - 0)) + 0;
-    var rand2 = Math.floor(Math.random() * (100 - rand)) + rand;
-    document.getElementById("show-div").innerHTML += rand + " + x = " + rand2 + "<br/>";
+}
+
+function hideDiv() {
+  // Hide div
+  var show = document.getElementById("show-div");
+
+  if (show !== null) {
+    show.setAttribute("id", "hide-div");
+  }
+}
+
+function showEquations() {
+  // Find primary action
+  var mathActionSelect = document.getElementById("math-action");
+  var userAction = mathActionSelect.options[mathActionSelect.selectedIndex].value;
+
+  // Determine number of equations
+  var numberOfResults = document.getElementById("number-of-equations").value;
+  var show = document.getElementById("show-div");
+
+  show.innerHTML = ""
+
+  if (userAction === "One Step Equations") {
+    if (isNaN(parseInt(numberOfResults))) {
+      numberOfResults = 10;
+    }
+    show.innerHTML += "Generating " + parseInt(numberOfResults) + " one step equations.";
+    show.innerHTML += "<br/><br/>";
+    
+    for (i = 0; i < parseInt(numberOfResults); i++) {
+      var rand1 = Math.floor((Math.random() * 100) + 0);
+      var rand2 = Math.floor((Math.random() * 100) + 0);
+      var operands = ['+', '-', '*', '/'];
+      var random_operand = operands[Math.floor(Math.random() * operands.length)];
+      show.innerHTML += rand1 + " " + random_operand + " x = " + rand2 + "<br/>";
+    }
+  }
+  else {
+    // Flashcards
+    show.innerHTML += "Here are some flashcards.";
   }
 }
